14e6507067a7749df67007835d77004b08b744e2
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-11-09 14:41:01 +0530

    Create Two_Sum.java

---
+++ Easy/Two_Sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement) && map.get(complement) !=i){
+                return (new int[] {i, map.get(complement)});
+            }
+            map.put(nums[i],i);
+        }/*
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement) && map.get(complement) !=i){
+                return (new int[] {i, map.get(complement)});
+            }
+        }*/
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
