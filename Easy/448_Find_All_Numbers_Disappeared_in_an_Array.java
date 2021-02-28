f30f90d9020b2cc38644778e2a50f22b182d5b9d
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2021-02-28 13:01:28 +0530

    Update 448_Find_All_Numbers_Disappeared_in_an_Array.java

--- Easy/448_Find_All_Numbers_Disappeared_in_an_Array.java
+++ Easy/448_Find_All_Numbers_Disappeared_in_an_Array.java
@@ -2,25 +2,39 @@ class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         
         List<Integer> arr = new ArrayList<Integer>();
-        Set<Integer> set = new HashSet<Integer>();
-        if(nums.length > 0){
-            int max = nums.length;
-            for(int i: nums){
-                set.add(i);
-            }
-            // for(int i: set){
-            //     System.out.println(i);
-            // }
-            for(int i=1;i<=max;i++){
-                if(!(set.contains(i))){
-                    arr.add(i);
+        if(nums.length>0){
+            for(int i=0;i<nums.length;i++){
+                int absoluteValue = Math.abs(nums[i]);
+                if(nums[absoluteValue -1] > 0){
+                    nums[absoluteValue -1] = -nums[absoluteValue -1];
                 }
             }
-            return arr;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i] > 0){
+                    arr.add((i+1));
+                }
+            }
         }
-        else{
-            return arr;
-        }
-        
+        return arr;
     }
 }
+
+        // Set<Integer> set = new HashSet<Integer>();
+        // if(nums.length > 0){
+        //     int max = nums.length;
+        //     for(int i: nums){
+        //         set.add(i);
+        //     }
+        //     // for(int i: set){
+        //     //     System.out.println(i);
+        //     // }
+        //     for(int i=1;i<=max;i++){
+        //         if(!(set.contains(i))){
+        //             arr.add(i);
+        //         }
+        //     }
+        //     return arr;
+        // }
+        // else{
+        //     return arr;
+        // }
