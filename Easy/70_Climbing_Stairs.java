96d17caabc93f9611c465775e28d4d0626f36d2e
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2021-03-06 11:29:42 +0530

    Update 70_Climbing_Stairs.java

--- Easy/70_Climbing_Stairs.java
+++ Easy/70_Climbing_Stairs.java
@@ -1,13 +1,31 @@
+// class Solution {
+    
+//     int arr[] = new int[46];
+//     public int climbStairs(int n) {
+//         if(n<0) return 0;
+//         if (n==0) return 1;
+//         if (arr[n] > 0) return arr[n];
+//         else{
+//             arr[n] =  ( climbStairs(n-2) + climbStairs(n-1) );
+//             return arr[n];
+//         }
+//     }
+// }
+
+
 class Solution {
     
-    int arr[] = new int[46];
     public int climbStairs(int n) {
-        if(n<0) return 0;
-        if (n==0) return 1;
-        if (arr[n] > 0) return arr[n];
-        else{
-            arr[n] =  ( climbStairs(n-2) + climbStairs(n-1) );
-            return arr[n];
+        if(n==1) return 1;
+        int arr[] = new int[n+1];
+        
+        arr[1] = 1;
+        arr[2] = 2;
+        
+        
+        for(int i=3;i<=n;i++){
+            arr[i]= arr[i-1] + arr[i-2];
         }
+        return arr[n];
     }
 }
